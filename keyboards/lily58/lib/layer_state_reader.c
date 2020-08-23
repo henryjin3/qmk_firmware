1e2816766397d48ebdbc1bf9deff78ded89e5fdc
henryjin3 <henry@cadenzadevelopment.com>
2020-08-23 16:29:04 -0500

    change oled display to show numpad

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -6,8 +6,7 @@
 #define L_BASE 0
 #define L_LOWER (1 << 1)
 #define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_NUMPAD (1 << 3)
 
 char layer_state_str[24];
 
@@ -23,9 +22,8 @@ const char *read_layer_state(void) {
   case L_LOWER:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
     break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+  case L_NUMPAD:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Numpad");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
